b5c45c34fc0d4fdc4714891fa31352aa54b021e5
YW Paul Kwon <young@berkeley.edu>
2018-01-28 12:57:23 -0800

    updated and checked with tensorflow 1.5, added colors to visualization

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ if os.path.isfile(model_ckpt):
 # start training
 if load: saver.restore(sess, './model')
 
-for step in range(100000):
+for step in range(50000):
     batch_x1, batch_y1 = mnist.train.next_batch(128)
     batch_x2, batch_y2 = mnist.train.next_batch(128)
     batch_y = (batch_y1 == batch_y2).astype('float')
