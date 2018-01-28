e6477fd78848b1fab5145b5e1ea086c15ae82b4a
YW Paul Kwon <young@berkeley.edu>
2018-01-28 12:50:13 -0800

    updated and checked with tensorflow 1.5, added colors to visualization

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 
 """ Siamese implementation using Tensorflow with MNIST example.
-This siamese network embeds a 28x28 image (a point in 784D) 
+This siamese network embeds a 28x28 image (a point in 784D)
 into a point in 2D.
 
 By Youngwook Paul Kwon (young at berkeley.edu)
@@ -9,6 +9,7 @@ By Youngwook Paul Kwon (young at berkeley.edu)
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from builtins import input
 
 #import system things
 from tensorflow.examples.tutorials.mnist import input_data # for data
@@ -31,41 +32,41 @@ saver = tf.train.Saver()
 tf.initialize_all_variables().run()
 
 # if you just want to load a previously trainmodel?
-new = True
-model_ckpt = 'model.ckpt'
+load = False
+model_ckpt = './model.meta'
 if os.path.isfile(model_ckpt):
     input_var = None
     while input_var not in ['yes', 'no']:
-        input_var = raw_input("We found model.ckpt file. Do you want to load it [yes/no]?")
+        input_var = input("We found model files. Do you want to load it and continue training [yes/no]?")
     if input_var == 'yes':
-        new = False
+        load = True
 
 # start training
-if new:
-    for step in range(100000):
-        batch_x1, batch_y1 = mnist.train.next_batch(128)
-        batch_x2, batch_y2 = mnist.train.next_batch(128)
-        batch_y = (batch_y1 == batch_y2).astype('float')
+if load: saver.restore(sess, './model')
 
-        _, loss_v = sess.run([train_step, siamese.loss], feed_dict={
-                            siamese.x1: batch_x1, 
-                            siamese.x2: batch_x2, 
-                            siamese.y_: batch_y})
+for step in range(100000):
+    batch_x1, batch_y1 = mnist.train.next_batch(128)
+    batch_x2, batch_y2 = mnist.train.next_batch(128)
+    batch_y = (batch_y1 == batch_y2).astype('float')
 
-        if np.isnan(loss_v):
-            print('Model diverged with loss = NaN')
-            quit()
+    _, loss_v = sess.run([train_step, siamese.loss], feed_dict={
+                        siamese.x1: batch_x1,
+                        siamese.x2: batch_x2,
+                        siamese.y_: batch_y})
 
-        if step % 10 == 0:
-            print ('step %d: loss %.3f' % (step, loss_v))
+    if np.isnan(loss_v):
+        print('Model diverged with loss = NaN')
+        quit()
 
-        if step % 1000 == 0 and step > 0:
-            saver.save(sess, 'model.ckpt')
-            embed = siamese.o1.eval({siamese.x1: mnist.test.images})
-            embed.tofile('embed.txt')
-else:
-    saver.restore(sess, 'model.ckpt')
+    if step % 10 == 0:
+        print ('step %d: loss %.3f' % (step, loss_v))
+
+    if step % 1000 == 0 and step > 0:
+        saver.save(sess, './model')
+        embed = siamese.o1.eval({siamese.x1: mnist.test.images})
+        embed.tofile('embed.txt')
 
 # visualize result
 x_test = mnist.test.images.reshape([-1, 28, 28])
-visualize.visualize(embed, x_test)
+y_test = mnist.test.labels
+visualize.visualize(embed, x_test, y_test)
--- visualize.py
+++ visualize.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import offsetbox
 
 
-def visualize(embed, x_test):
+def visualize(embed, x_test, y_test):
 
     # two ways of visualization: scale to fit [0,1] scale
     # feat = embed - np.min(embed, 0)
@@ -19,14 +19,18 @@ def visualize(embed, x_test):
 
     plt.figure()
     ax = plt.subplot(111)
+    colormap = plt.get_cmap('tab10')
     shown_images = np.array([[1., 1.]])
     for i in range(feat.shape[0]):
         dist = np.sum((feat[i] - shown_images)**2, 1)
         if np.min(dist) < 3e-4*ax_dist_sq:   # don't show points that are too close
             continue
         shown_images = np.r_[shown_images, [feat[i]]]
+        patch_to_color = np.expand_dims(x_test[i], -1)
+        patch_to_color = np.tile(patch_to_color, (1, 1, 3))
+        patch_to_color = (1-patch_to_color) * (1,1,1) + patch_to_color * colormap(y_test[i]/10.)[:3]
         imagebox = offsetbox.AnnotationBbox(
-            offsetbox.OffsetImage(x_test[i], zoom=0.6, cmap=plt.cm.gray_r),
+            offsetbox.OffsetImage(patch_to_color, zoom=0.5, cmap=plt.cm.gray_r),
             xy=feat[i], frameon=False
         )
         ax.add_artist(imagebox)
@@ -40,9 +44,10 @@ if __name__ == "__main__":
 
     mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
     x_test = mnist.test.images
+    y_test = mnist.test.labels
     x_test = x_test.reshape([-1, 28, 28])
 
     embed = np.fromfile('embed.txt', dtype=np.float32)
     embed = embed.reshape([-1, 2])
 
-    visualize(embed, x_test)
+    visualize(embed, x_test, y_test)
